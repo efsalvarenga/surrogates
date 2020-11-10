40414c4dabdcb271964affabbed9a7edbc4bdb20
Estevao.Alvarenga <estevao.alvarenga@shell.com>
2020-11-10 08:46:15 +0100

    Update rsm.R

--- src/rsm.R
+++ src/rsm.R
@@ -101,3 +101,94 @@ par(mfrow=c(1,2))
 persp(x1, x2, eta2s, theta=30, phi=30, zlab="eta", expand=0.75, lwd=0.25)
 image(x1, x2, eta2s, col=heat.colors(128))
 contour(x1, x2, eta2s, add=TRUE)
+
+# Aircraft wing weight example
+wingwt <- function(Sw=0.48, Wfw=0.4, A=0.38, L=0.5, q=0.62, l=0.344, 
+                   Rtc=0.4, Nz=0.37, Wdg=0.38)
+{
+  ## put coded inputs back on natural scale
+  Sw <- Sw*(200 - 150) + 150 
+  Wfw <- Wfw*(300 - 220) + 220 
+  A <- A*(10 - 6) + 6 
+  L <- (L*(10 - (-10)) - 10) * pi/180
+  q <- q*(45 - 16) + 16 
+  l <- l*(1 - 0.5) + 0.5 
+  Rtc <- Rtc*(0.18 - 0.08) + 0.08
+  Nz <- Nz*(6 - 2.5) + 2.5
+  Wdg <- Wdg*(2500 - 1700) + 1700
+  
+  ## calculation on natural scale
+  W <- 0.036*Sw^0.758 * Wfw^0.0035 * (A/cos(L)^2)^0.6 * q^0.006 
+  W <- W * l^0.04 * (100*Rtc/cos(L))^(-0.3) * (Nz*Wdg)^(0.49)
+  return(W)
+}
+
+x <- seq(0, 1, length=100)
+g <- expand.grid(x, x)
+
+W.A.Nz <- wingwt(A=g[,1], Nz=g[,2])
+
+cs <- heat.colors(128)
+bs <- seq(min(W.A.Nz), max(W.A.Nz), length=129)
+
+image(x, x, matrix(W.A.Nz, ncol=length(x)), col=cs, breaks=bs, 
+      xlab="A", ylab="Nz")
+contour(x, x, matrix(W.A.Nz, ncol=length(x)), add=TRUE)
+
+W.l.Wfw <- wingwt(l=g[,1], Wfw=g[,2])
+
+image(x, x, matrix(W.l.Wfw,ncol=length(x)), col=cs, breaks=bs, 
+      xlab="l", ylab="Wfw")
+contour(x,x, matrix(W.l.Wfw,ncol=length(x)), add=TRUE)
+
+library(lhs)
+n <- 1000
+X <- data.frame(randomLHS(n, 9))
+names(X) <- names(formals(wingwt))
+
+plot(X[,1:2], pch=19, cex=0.5)
+abline(h=c(0.6, 0.8), col=2, lwd=2)
+
+inbox <- X[,1] > 0.6 &  X[,1] < 0.8 
+sum(inbox)/nrow(X)
+
+Y <- wingwt(X[,1], X[,2], X[,3], X[,4], X[,5], X[,6], X[,7], X[,8], X[,9])
+
+fit.lm <- lm(log(Y) ~ .^2, data=data.frame(Y,X))
+fit.lmstep <- step(fit.lm, scope=formula(fit.lm), direction="backward", 
+                   k=log(length(Y)), trace=0)
+
+library(laGP)
+fit.gp <- newGPsep(X, Y, 2, 1e-6, dK=TRUE)
+mle <- mleGPsep(fit.gp)
+
+baseline <- matrix(rep(as.numeric(formals(wingwt)), nrow(g)), 
+                   ncol=9, byrow=TRUE)
+XX <- data.frame(baseline)
+names(XX) <- names(X)
+XX$A <- g[,1]
+XX$Nz <- g[,2]
+
+p <- predGPsep(fit.gp, XX, lite=TRUE)
+
+image(x, x, matrix(p$mean, ncol=length(x)), col=cs, breaks=bs, 
+      xlab="A", ylab="Nz")
+contour(x, x, matrix(p$mean, ncol=length(x)), add=TRUE)
+
+meq1 <- meq2 <- me <- matrix(NA, nrow=length(x), ncol=ncol(X))
+for(i in 1:ncol(me)) {
+  XX <- data.frame(baseline)[1:length(x),]
+  XX[,i] <- x
+  p <- predGPsep(fit.gp, XX, lite=TRUE)
+  me[,i] <- p$mean
+  meq1[,i] <- qt(0.05, p$df)*sqrt(p$s2) + p$mean
+  meq2[,i] <- qt(0.95, p$df)*sqrt(p$s2) + p$mean
+}
+
+matplot(x, me, type="l", lwd=2, lty=1, col=1:9, xlab="coded input")
+matlines(x, meq1, type="l", lwd=2, lty=2, col=1:9)
+matlines(x, meq2, type="l", lwd=2, lty=2, col=1:9)
+legend("topleft", names(X)[1:5], lty=1, col=1:5, horiz=TRUE, 
+       bty="n", cex=0.5)
+legend("bottomright", names(X)[6:9], lty=1, col=6:9, horiz=TRUE, 
+       bty="n", cex=0.5)
