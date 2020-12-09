9bf79e88bb47c3d1d775b8e027033add2e83a83a
Estevao.Alvarenga <estevao.alvarenga@shell.com>
2020-12-09 21:53:27 +0100

    finished chap 2 ex b

--- src/chap2.R
+++ src/chap2.R
@@ -6,6 +6,7 @@ library(akima)
 library(ggplot2)
 library(laGP)
 library(olsrr)
+library(lhs)
 
 # -------------------------------------------------------------------------------------------------
 # Rocket booster dynamics
@@ -282,6 +283,77 @@ p <- predGPsep(fit.gp, XX, lite=TRUE)
 rmspe <- sqrt(mean((100*(p$mean - test[,8])/test[,8])^2))
 rmspe
 
+# Homework exercises
+
+# a. Get familiar with using tpm to calculate satellite drag coefficients. Begin by compiling
+# the C code to produce a shared object for use with the R wrapper in tpm-git/tpm/R/tpm.R.
+# Run R CMD SHLIB -o tpm.so *.c in the tpm-git/tpm/src directory. Then double-check you have a
+# working tpm library by trying some of the examples in §2.3.2.
+
+train[1,]
+exa01 <- tpm(train[1, 1:7], moles = moles, stl = mesh, verb = 1)
+exa01
+
+train[12,]
+exa02 <- tpm(train[12, 1:7], moles = moles, stl = mesh, verb = 1)
+exa02
+
+# b. Generate a random one-hundred run, 7d testing design uniformly in the ranges provided by
+# Table 2.5 with restricted angle inputs as described in Table 2.6. Evaluate these inputs for
+# GRACE under the mole fractions provided in §2.3.2 for a particular LEO position.
+
+# create unit cube lhs
+dimSize <- 7
+exb01 <- data.frame(rbind(rep(0, dimSize),
+                          randomLHS(98, 7),
+                          rep(1, dimSize)))
+names(exb01) <- names(train)[1:7]
+
+# put cube lhs in right ranges
+exb01 <- sapply(names(exb01), function(col){
+  exb01[[col]] * (r[2, col] - r[1, col]) + r[1, col]
+})
+apply(exb01[,1:7], 2, range)
+r
+
+# run TPM
+if (!file.exists('./data/exb01Y.csv')) {
+  exb01Y <- tpm(exb01, moles = moles, stl = mesh, verb = 1)
+  write.csv(exb01Y, './data/exb01Y.csv', row.names = FALSE)
+} else {
+  read.csv('./data/exb01Y.csv')
+}
+
+# c. Train six GP surrogates on the pure species data provided in the directory tpm-git/data/GRACE.
+# A few helpful notes: pure He was done already (see §2.3.2), so there are only five left to do;
+# be careful to use the same coding scheme for all six sets of inputs; you may wish to double-check
+# your pure species predictors on the pure species testing sets provided.
+
+
+
+
+
+
+
+
+Collect predictions from all six GP surrogates at the testing locations from #b. Combine them into a single prediction for that LEO position and calculate RMSPE to the true simulation outputs from #b. Is the RMSPE close to 1%?
+Repeat #b–d for HST with an identical setup except that you’ll need to augment your design with 100 random panel angles in  
+{
+  0
+  ,
+  10
+  ,
+  20
+  ,
+  …
+  ,
+  80
+  ,
+  90
+}
+and utilize the appropriate mesh files in your simulations.
+
+# -------------------------------------------------------------------------------------------------
 # Groundwater remediation
 bvv <- read.csv("runlock/pato_results.csv")
 cols <- 3:14
