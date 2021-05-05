873da8e6604f2a5c15ceac61c74787c4f13aa8d0
Estevao.Alvarenga <estevao.alvarenga@shell.com>
2021-05-05 10:01:21 +0200

    continued on chap 3

--- src/chap3.R
+++ src/chap3.R
@@ -219,3 +219,139 @@ plot(rs, p$fit, type="b", ylim=c(20,100), xlab="radius (R)",
      ylab="y.hat(x) & 95% CIs")
 lines(rs, p$fit + 2*p$se, col=2, lty=2)
 lines(rs, p$fit - 2*p$se, col=2, lty=2)
+
+# investigating sampling properties
+# creating a 2d subset of the chemical saddle data
+x12 <- seq(-2,2,length=100)
+g <- expand.grid(x12, x12)
+Xp <- data.frame(x1=g[,1], x2=g[,2], 
+                 x11=g[,1]^2, x22=g[,2]^2, x12=g[,1]*g[,2])
+
+## x3 = x4 = 0
+Xp$x3 <- Xp$x33 <- Xp$x4 <- Xp$x44 <- Xp$x34 <- 0
+Xp$x13 <- Xp$x14 <- Xp$x23 <- Xp$x24 <- Xp$x34 <- 0
+p0 <- predict(fit, newdata=Xp, se.fit=TRUE)
+
+## x3 = x4 = 1
+Xp$x3 <- Xp$x33 <- Xp$x4 <- Xp$x44 <- Xp$x34 <- 1
+Xp$x13 <- Xp$x14 <- Xp$x1; Xp$x23 <- Xp$x24 <- Xp$x2
+p1 <- predict(fit, newdata=Xp, se.fit=TRUE)
+
+# produce plot
+par(mfrow=c(1,2))
+bs <- seq(min(p0$se.fit), max(p0$se.fit), length=129)
+image(x12, x12, matrix(p0$se.fit, nrow=length(x12)), col=cols, breaks=bs,
+      xlab="x1", ylab="x2", main="se.fit, x3 = x4 = 0")
+contour(x12, x12, matrix(p0$se.fit, nrow=length(x12)), add=TRUE)
+points(saddle[,1:2])
+points(saddle[apply(saddle[,3:4] == c(0,0), 1, all),1:2], pch=19)
+image(x12, x12, matrix(p1$se.fit, nrow=length(x12)), col=cols, breaks=bs,
+      xlab="x1", ylab="x2", main="se.fit, x3 = x4 = 1")
+contour(x12, x12, matrix(p1$se.fit, nrow=length(x12)), add=TRUE)
+points(saddle[,1:2])
+points(saddle[apply(saddle[,3:4] == c(1,1), 1, all), 1:2], pch=19)
+
+# confidence in the sationaty point
+crdat <- read.table("data/confreg.txt", header=TRUE)
+
+# code below combines several steps: expanding to second-order features,
+# model fitting and extracting b and B, differentiating and solving to
+# estimate stationary point xs
+crdat$x11 <- crdat$x1^2
+crdat$x22 <- crdat$x2^2
+crdat$x12 <- crdat$x1 * crdat$x2
+fit <- lm(y ~ ., data=crdat)
+summary(fit)
+b <- coef(fit)[2:3]
+B <- matrix(NA, nrow=2, ncol=2)
+diag(B) <- coef(fit)[4:5]
+B[1,2] <- B[2,1] <- coef(fit)[6]/2
+xs <- -(1/2)*solve(B, b)
+xs
+eigen(B)$values # stationary point is a max
+
+# Next evaluate predictive equations on a grid in order to visualize the
+# response surface, design and stationary point, which is well within
+# the interior of the experimental region
+par(mfrow=c(1,1))
+xx <- seq(-1.6, 1.6, length=200)
+g <- expand.grid(xx, xx)
+XX <- data.frame(x1=g[,1], x2=g[,2], 
+                 x11=g[,1]^2, x22=g[,2]^2, x12=g[,1]*g[,2])
+p <- as.numeric(predict(fit, newdata=XX))
+image(xx, xx, matrix(p, nrow=length(xx)), col=cols, xlab="x1", ylab="x2")
+contour(xx, xx, matrix(p, nrow=length(xx)), add=TRUE)
+points(crdat$x1, crdat$x2, pch=20)
+points(xs[1], xs[2])
+text(xs[1], xs[2], "xs", pos=4)
+
+# Combining  d(t) = b + 2Bt
+d <- rbind(c(b[1], 2*B[,1]), c(b[2], 2*B[,2]))
+colnames(d) <- c("(Intercept)", "t1", "t2")
+d
+
+# variance of d(t)
+s2 <- summary(fit)$sigma^2
+s2
+
+# on n - p = 7 DoF
+X <- cbind(1, as.matrix(crdat[,-3]))
+XtXi <- solve(t(X) %*% X)
+XtXi
+
+# function to determine the covariance matrix as a function of its elements,
+# and coordinates t
+Vard <- function(t1, t2, s2, XtXi) 
+{
+  v11 <- XtXi[2,2] + 4*t1^2*XtXi[4,4] + t2^2*XtXi[6,6]
+  v22 <- XtXi[3,3] + t1^2*XtXi[6,6] + 4*t2^2*XtXi[5,5]
+  v12 <- v21 <- 4*t1*t2*XtXi[4,5] + t1*t2*XtXi[6,6]
+  v <- s2 * matrix(c(v11,v12,v21,v22), ncol=2, byrow=TRUE)
+  return(v)
+}
+
+# quadratic form of Eq 3.7
+CIqf <- function(t1, t2, s2, XtXi, b, B)
+{
+  dt <- b + 2*B %*% c(t1, t2)
+  V <- Vard(t1, t2, s2, XtXi)
+  Vi <- solve(V)
+  t(dt) %*% Vi %*% dt
+}
+
+# evaluate Eq 3.8
+quadform <- rep(NA, nrow(g))
+for(i in 1:nrow(g)) quadform[i] <- CIqf(g[i,1], g[i,2], s2, XtXi, b, B)
+
+ci90 <- quadform <= 2*qf(0.9, 2, nrow(X)-ncol(X))
+ci95 <- quadform <= 2*qf(0.95, 2, nrow(X)-ncol(X))
+image(xx, xx, matrix(ci90 + ci95, ncol=length(xx)), xlab="x1", ylab="x2", 
+      col=c("white", "lightgray", "darkgray"))
+text(c(-0.2,-1), c(-0.5,1), c("90%","95%"))
+
+summary(fit)$r.squared
+
+# artificially improving the dataset to see response of CI
+crdat$y <- c(87.6, 86.5, 85.7, 86.9, 86.7, 86.8, 87.4, 86.7, 90.3, 
+             91.0, 90.8) 
+fit2 <- lm(y ~ ., data=crdat)
+summary(fit2)$r.squared # much better fit
+
+# updating calculations
+b <- coef(fit)[2:3]
+B <- matrix(NA, nrow=2, ncol=2)
+diag(B) <- coef(fit)[4:5]
+B[1,2] <- B[2,1] <- coef(fit)[6]/2
+xs <- -(1/2)*solve(B, b)
+s2 <- summary(fit2)$sigma^2
+for(i in 1:nrow(g)) quadform[i] <- CIqf(g[i,1], g[i,2], s2, XtXi, b, B)
+
+ci95 <- quadform <= 2*qf(0.95, 2, nrow(X)-ncol(X))
+image(xx, xx, matrix(ci95, ncol=length(xx)), xlab="x1", ylab="x2",
+      col=c("white", "lightgray"))
+
+# IMPORTANT REMARK
+# This state of affairs is merely a reflection of the reality that the response
+# surface is locally flat. A positive spin may be that such situations imply a
+# degree of flexibility in choosing the optimum. Communicating that effectively
+# to stakeholders requires care, of course, but is definitely of interest.
